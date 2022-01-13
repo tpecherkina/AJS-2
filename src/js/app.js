04b8a397d07812e0ccd0d37eae4cfa8d3e4e7bf8
tpecherkina <tpecherkina18@gmail.com>
2022-01-12 23:31:48 -0500

    init

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import GameSavingData, { readGameSaving as loadGame, writeGameSaving as saveGame, Game } from './game.js';
+
+const game = new Game();
+
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,16 @@
+import character from './domain.js'
+
+export class Game {
+	start() {
+		console.log('game started');
+	}
+}
+
+export default class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
